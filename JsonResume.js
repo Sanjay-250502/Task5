4deaeb50243bbb4cf0295c73083aefe886f2b6fb
Sanjay-250502 <sanjayramachandran2002@gmail.com>
2023-10-27 01:40:46 +0530

    Task5 updated

---
+++ JsonResume.js
@@ -0,0 +1,43 @@
+//! 2.Create your own resume data in JSON format.
+const sanResume = {
+"personalDetails":[
+        {
+        "name" :"Sanjay R J",
+        "Email" : "sanjayramachandran2002@gmail.com",
+        "Phone Number":"+91 0123456789",
+        "Address": "56256 street,city,state",
+        "state":"Tamilnadu"
+} 
+],
+"education": [
+    {
+        "Degree": "BE in Computer Science and Engineering",
+        "Institution": "CMS College of Engineering and Technology",
+        "CGPA":"8.1",
+        "Graduated" : "2023"
+    }
+],
+"Skills":[
+    {
+        "Front-end" : "HTML,CSS,BOOTSTRAP,JAVASCRIPT,REACTJS",
+        "Back-End" : "MYSQL,MONGODB,EXPRESSJS,NODEJS"
+    }
+],
+"languagesKnown":[
+    {
+        "language":"Tamil,English,Malayalam"
+    }
+],
+"certification":[
+    {
+    "Mern":"Certified MERN Stack Developer",
+    "AI" :"AI for India 2.0"
+    }
+]
+}
+// console.log(sanResume);
+// console.log(sanResume["personalDetails"])
+// console.log(sanResume["education"])
+// console.log(sanResume["skills"])
+// console.log(sanResume["languagesKnown"])
+// console.log(sanResume["certification"])
