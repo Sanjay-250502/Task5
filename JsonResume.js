2935cca0889b1d5981a33f522013390b3f4f2205
Sanjay-250502 <sanjayramachandran2002@gmail.com>
2024-02-02 01:16:06 +0530

    code updated

--- JsonResume.js
+++ JsonResume.js
@@ -1,4 +1,5 @@
-//! 2.Create your own resume data in JSON format.
+//! 1.Create your own resume data in JSON format.
+
 const sanResume = {
 "personalDetails":[
         {
@@ -35,9 +36,38 @@ const sanResume = {
     }
 ]
 }
-// console.log(sanResume);
-// console.log(sanResume["personalDetails"])
-// console.log(sanResume["education"])
-// console.log(sanResume["skills"])
-// console.log(sanResume["languagesKnown"])
-// console.log(sanResume["certification"])
+console.log(sanResume);
+console.log(sanResume["personalDetails"])
+console.log(sanResume["education"])
+console.log(sanResume["skills"])
+console.log(sanResume["languagesKnown"])
+console.log(sanResume["certification"])
+
+
+//! 2.For the given JSON iterate over all for loops(for,for in,for of,forEach)
+let data={
+    name:"xxx",
+    age:21
+}
+console.log("For-loop");
+const keys=Object.keys(data)
+for (let i = 0; i < keys.length; i++) {
+    console.log(keys[i]+":"+data[keys[i]]);
+    
+}
+console.log(" " );
+console.log("For-in");
+for (let key in data) {
+    console.log(key + ": " + data[key]);
+  }
+console.log(" ");
+const values=Object.entries(data)
+console.log("For-of");
+for(let [key,val] of values){
+    console.log(key+":"+val);
+}
+console.log("");
+console.log("For-each");
+Object.entries(data).forEach(([key,val])=>{
+    console.log(key +":"+val);
+})
